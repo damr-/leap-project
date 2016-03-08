742f512c0f4c9ac1c1716c648b4db6d64d59c042
admin <admin>
2016-03-08 14:40:49 +0100

    added ability to restart level

--- Assets/PXL/Scripts/Utility/LevelInput.cs
+++ Assets/PXL/Scripts/Utility/LevelInput.cs
@@ -1,24 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace PXL.Utility {
 
-	public class QuitInput : MonoBehaviour {
+	public class LevelInput : MonoBehaviour {
 	
 		/// <summary>
 		/// The key used for quitting the application
 		/// </summary>
 		public KeyCode quitKey = KeyCode.Escape;
+
+		/// <summary>
+		/// The key used for restarting the level
+		/// </summary>
+		public KeyCode restartKey = KeyCode.Space;
 		
 		/// <summary>
 		/// Within how many seconds the second tap has to follow
 		/// </summary>
 		public float maxDoubleTapDelay = 1f;
 
+		/// <summary>
+		/// Last time the escape key was pressed
+		/// </summary>
 		private float lastTime;
 
 		private bool pressedOnce = false;
 
 		private void Update() {
+			if (Input.GetKeyDown(restartKey)) {
+				Scene scene = SceneManager.GetActiveScene();
+				SceneManager.LoadScene(scene.name);
+			}
+
 			if (Input.GetKeyDown(quitKey)) {
 				if (!pressedOnce) {
 					lastTime = Time.time;
