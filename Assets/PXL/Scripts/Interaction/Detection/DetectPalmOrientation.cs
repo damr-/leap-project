d3781ff4caf9d6f51298e7a85be39518e1e4baee
David Weichselbaum <david.weichselbaum@hotmail.com>
2016-04-15 13:20:33 +0200

    palm rotation detection is now with rotation vector

--- Assets/PXL/Scripts/Interaction/Detection/DetectPalmOrientation.cs
+++ Assets/PXL/Scripts/Interaction/Detection/DetectPalmOrientation.cs
@@ -1,29 +1,21 @@
 ﻿using UnityEngine;
 using Leap.Unity;
+using UnityEditor;
 
 namespace PXL.Interaction.Detection {
 
 	public class DetectPalmOrientation : HandDetector {
 
-		/// <summary>
-		/// The direction the normal of the hand's palm should be close to
-		/// </summary>
-		public Vector3 TargetDirection = Vector3.down;
+		public Vector3 TargetRotation = Vector3.down;
 
-		/// <summary>
-		/// How many degrees away from the ideal <see cref="TargetDirection"/> the orientation is taken as correctly oriented
-		/// </summary>
 		public float AngleError = 30f;
 
+		public bool PreviewTargetRotation = true;
+
 		protected override void CheckHand(HandModel hand) {
-			var leapHand = TryGetLeapHand(hand);
-			if (leapHand == null)
-				return;
+			var angle = Quaternion.Angle(hand.palm.localRotation, Quaternion.Euler(TargetRotation));
 
-			var palmNormal = leapHand.PalmNormal.ToVector3();
-			var palmAngle = Vector3.Angle(TargetDirection, palmNormal);
-
-			if (palmAngle < 30) {
+			if (angle < AngleError) {
 				TryInvokeCorrect(hand);
 			}
 			else {
@@ -31,6 +23,64 @@ namespace PXL.Interaction.Detection {
 			}
 		}
 
+		void OnDrawGizmos() {
+			if (!PreviewTargetRotation)
+				return;
+
+			foreach (var handModel in HandModels) {
+				if (handModel == null)
+					continue;
+
+				var palm = handModel.palm;
+				var palmRot = palm.rotation.eulerAngles;
+				var targetRotation = Quaternion.Euler(TargetRotation);
+				var qDiff = targetRotation * Quaternion.Inverse(palm.rotation);
+
+				transform.position = palm.position;
+				transform.localRotation = targetRotation;
+
+				DrawTargetRotation(palm.position, transform.up, new Color(76 / 255f, 176 / 255f, 76 / 255f));
+				DrawTargetRotation(palm.position, transform.right, new Color(176 / 255f, 76 / 255f, 76 / 255f));
+				DrawTargetRotation(palm.position, transform.forward, new Color(76 / 255f, 76 / 255f, 176 / 255f));
+			}
+		}
+
+		private void DrawTargetRotation(Vector3 origin, Vector3 direction, Color color) {
+			var len = 0.25f;
+
+			Gizmos.color = color;
+
+			direction = direction.normalized * len;
+
+			Gizmos.DrawRay(origin, direction);
+
+			Gizmos.color = new Color(color.r / 2f, color.g / 2f, color.b / 2f);
+
+			//for (var i = 0; i < 359; i++) {
+			//	var x = 0.145f * Mathf.Cos(i);
+			//	var z = 0.145f * AngleError * Mathf.Sin(i);
+			//	Gizmos.DrawRay(origin, center + new Vector3(x, 0, z));
+			//}
+
+			Gizmos.DrawRay(origin, direction);
+		}
+
 	}
 
 }
+
+//				if (handModel == null)
+//					continue;
+//				var palm = handModel.palm;
+//var qRot = Quaternion.Euler(TargetRotation);
+//var palmRot = palm.rotation.eulerAngles;
+
+//var diff = qRot * Quaternion.Inverse(palm.rotation);
+
+//var up = (Quaternion.Euler(palm.up) * diff).eulerAngles;
+//var right = (Quaternion.Euler(palm.right) * diff).eulerAngles;
+//var forward = (Quaternion.Euler(palm.forward) * diff).eulerAngles;
+
+//				DrawTargetRotation(palm.position, up, new Color(76 / 255f, 176 / 255f, 76 / 255f));
+//				DrawTargetRotation(palm.position, right, new Color(176 / 255f, 76 / 255f, 76 / 255f));
+//				DrawTargetRotation(palm.position, forward, new Color(76 / 255f, 76 / 255f, 176 / 255f));
