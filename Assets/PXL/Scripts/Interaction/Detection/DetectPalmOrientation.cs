cf8fafc3f42ee72271652927b7aa047dc438f6eb
David Weichselbaum <david.weichselbaum@hotmail.com>
2016-04-15 15:13:32 +0200

    improved orientation preview rendering

--- Assets/PXL/Scripts/Interaction/Detection/DetectPalmOrientation.cs
+++ Assets/PXL/Scripts/Interaction/Detection/DetectPalmOrientation.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using Leap.Unity;
-using UnityEditor;
 
 namespace PXL.Interaction.Detection {
 
@@ -11,6 +10,14 @@ namespace PXL.Interaction.Detection {
 		public float AngleError = 30f;
 
 		public bool PreviewTargetRotation = true;
+
+		private const float LineLength = 0.25f;
+
+		private readonly Color green = new Color(76 / 255f, 176 / 255f, 76 / 255f);
+
+		private readonly Color red = new Color(176 / 255f, 76 / 255f, 76 / 255f);
+
+		private readonly Color blue = new Color(76 / 255f, 76 / 255f, 176 / 255f);
 
 		protected override void CheckHand(HandModel hand) {
 			var angle = Quaternion.Angle(hand.palm.localRotation, Quaternion.Euler(TargetRotation));
@@ -32,55 +39,21 @@ namespace PXL.Interaction.Detection {
 					continue;
 
 				var palm = handModel.palm;
-				var palmRot = palm.rotation.eulerAngles;
-				var targetRotation = Quaternion.Euler(TargetRotation);
-				var qDiff = targetRotation * Quaternion.Inverse(palm.rotation);
-
 				transform.position = palm.position;
-				transform.localRotation = targetRotation;
+				transform.localRotation = Quaternion.Euler(TargetRotation);
 
-				DrawTargetRotation(palm.position, transform.up, new Color(76 / 255f, 176 / 255f, 76 / 255f));
-				DrawTargetRotation(palm.position, transform.right, new Color(176 / 255f, 76 / 255f, 76 / 255f));
-				DrawTargetRotation(palm.position, transform.forward, new Color(76 / 255f, 76 / 255f, 176 / 255f));
+				DrawTargetRotation(palm.position, transform.up, green);
+				DrawTargetRotation(palm.position, transform.right, red);
+				DrawTargetRotation(palm.position, transform.forward, blue);
 			}
 		}
 
 		private void DrawTargetRotation(Vector3 origin, Vector3 direction, Color color) {
-			var len = 0.25f;
-
 			Gizmos.color = color;
-
-			direction = direction.normalized * len;
-
-			Gizmos.DrawRay(origin, direction);
-
-			Gizmos.color = new Color(color.r / 2f, color.g / 2f, color.b / 2f);
-
-			//for (var i = 0; i < 359; i++) {
-			//	var x = 0.145f * Mathf.Cos(i);
-			//	var z = 0.145f * AngleError * Mathf.Sin(i);
-			//	Gizmos.DrawRay(origin, center + new Vector3(x, 0, z));
-			//}
-
+			direction = direction.normalized * LineLength;
 			Gizmos.DrawRay(origin, direction);
 		}
 
 	}
 
 }
-
-//				if (handModel == null)
-//					continue;
-//				var palm = handModel.palm;
-//var qRot = Quaternion.Euler(TargetRotation);
-//var palmRot = palm.rotation.eulerAngles;
-
-//var diff = qRot * Quaternion.Inverse(palm.rotation);
-
-//var up = (Quaternion.Euler(palm.up) * diff).eulerAngles;
-//var right = (Quaternion.Euler(palm.right) * diff).eulerAngles;
-//var forward = (Quaternion.Euler(palm.forward) * diff).eulerAngles;
-
-//				DrawTargetRotation(palm.position, up, new Color(76 / 255f, 176 / 255f, 76 / 255f));
-//				DrawTargetRotation(palm.position, right, new Color(176 / 255f, 76 / 255f, 76 / 255f));
-//				DrawTargetRotation(palm.position, forward, new Color(76 / 255f, 76 / 255f, 176 / 255f));
